ae8f054ab9d39a6cbb2d5385e412efb6b0237adb
ARK <123211383+Arko23@users.noreply.github.com>
2024-02-21 16:53:29 +0600

    hey man

---
+++ js/page.js
@@ -0,0 +1,8 @@
+function begin()
+{
+    const homeScreen=document.getElementById('home');
+    homeScreen.classList.add('hidden');
+
+    const nextScreen=document.getElementById('next');
+    nextScreen.classList.remove('hidden');
+}
